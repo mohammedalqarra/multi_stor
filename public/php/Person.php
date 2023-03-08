59672e43b2dcd1ed90878b2eeeda3a536fa9b663
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-08 10:39:19 +0200

    init:commit test php file Person.php

--- public/php/Person.php
+++ public/php/Person.php
@@ -7,11 +7,21 @@ class Person
     protected $gender;
     private $age;
 
+    public static $country;
+
     public function setAge($age){
         $this->age = $age;
         return $this;
         // $this عبارة عن reference لل object إلي بننشئه داخل هذا ال class  وعبارة عن variable يبدأ بال $
     }
+
+    // static method ما بينفع أستخدم بداخلها $this  لانوا دائما" تعود على ال reference
+    // في حال ال static properties  لازم أضع ال $
+
+    public static function setCountry($country){
+            salf::$country = $country;
+        }
+
 }
 
 ?>
