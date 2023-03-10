a6dabd45328223a2eea6b4769b88ffddd3a7554a
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-10 15:06:06 +0200

    init:commit add dashboardcontroller

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('admin.dashboard');
-});
+// Route::get('/', function () {
+//     return view('admin.dashboard');
+// });
+
