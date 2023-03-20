9ea93528c88cbe74527604c15e47c04e265d17de
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-20 20:17:55 +0200

    init:commit add breeze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="{{ asset('../../css/app.css') }}">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -32,5 +33,7 @@
                 {{ $slot }}
             </main>
         </div>
+
+        <script src="{{ asset('../../js/app.js') }}"></script>
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
         <link rel="stylesheet" href="{{ asset('../../css/app.css') }}">
     </head>
     <body class="font-sans text-gray-900 antialiased">
@@ -27,5 +27,6 @@
                 {{ $slot }}
             </div>
         </div>
+        <script src="{{ asset('../../js/app.js') }}"></script>
     </body>
 </html>
