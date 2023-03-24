a734af5ad97d198f0db6693901458212999de07b
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-24 23:58:14 +0200

    init:commit ui avatars

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -185,7 +185,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 @auth
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
-                            <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                            <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name  }}" class="img-circle elevation-2"
                                 alt="User Image">
                         </div>
                         <div class="info">
