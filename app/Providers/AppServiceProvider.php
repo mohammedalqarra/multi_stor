7106ef42eb61c22d9b67ef9ac330443bae7e2c08
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-24 17:33:08 +0200

    initcommit add Paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-      //  Schema::defaultStringLength(191);
-     //   Paginator::useBootstrapFive();
-      //  Paginator::useBootstrapFour();
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrapFive();
+        Paginator::useBootstrapFour();
     }
 }
