8bb85f63cde2acca6170cd054054f243f9637afe
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-04-05 08:38:04 +0300

    init:commit add validate

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -47,6 +47,18 @@ class CategoriesController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'name' =>  'required|string|min:3|max:255',
+            'parent_id' => [
+                'int', 'exists:categories,id'
+            ],
+
+            'image' => [
+                'image' , 'max:1048576' , 'dimensions:min_width=100,min_height=100',
+            ],
+
+            'status' => 'in:active,archived',
+        ]);
 
         //Request merge
         $request->merge([
