8606add797d97a453af6ad8c68dbb953d56551fd
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-04-10 22:47:22 +0300

    init:commit add invalid error

--- resources/views/admin/categories/_form.blade.php
+++ resources/views/admin/categories/_form.blade.php
@@ -15,35 +15,43 @@
     @enderror --}}
 
 
-    <x-form.input label="Category Name" name="name" class="form-control-lg" role="input" :value="$category->name" />
-</div>
-<div class="form-group">
-    <label for="">Category Parent</label>
-    <select name="parent_id" class="form-control form-select">
-        <option value="">Primary Category</option>
-        @foreach ($parents as $parent)
-            <option value="{{ $parent->id }}" @selected(old('parent_id', $category->parent_id) == $parent->id)>{{ $parent->name }}</option>
-        @endforeach
-    </select>
+    <x-form.input label="Category Name" name="name" class="form-control-lg"
+        role="input" :value="$category->name" />
 
-</div>
-<div class="form-group">
-    <x-form.textarea name="description" label="Description" class="myeditor" rows="10" :value="$category->description" />
-</div>
-<div class="form-group">
-    <x-form.label id="image">Image</x-form.label>
-    <x-form.input  type="file" id="image" name="image" class="form-control" accept="image/*" />
-    @if ($category->image)
-        <img src="{{ asset('storage/' . $category->image) }}" class="w-25" alt="50">
-    @endif
-</div>
-<div class="form-group">
-    {{-- <label for="">Status</label> --}}
+    @error('name')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <label for="">Category Parent</label>
+        <select name="parent_id" class="form-control form-select">
+            <option value="">Primary Category</option>
+            @foreach ($parents as $parent)
+                <option value="{{ $parent->id }}" @selected(old('parent_id', $category->parent_id) == $parent->id)>{{ $parent->name }}</option>
+            @endforeach
+        </select>
+
+    </div>
+    <div class="form-group">
+        <x-form.textarea name="description" label="Description" class="myeditor" rows="10" :value="$category->description" />
+    </div>
+    <div class="form-group">
+        <x-form.label id="image">Image</x-form.label>
+        <x-form.input type="file" id="image" name="image" class="form-control" accept="image/*" />
+        @if ($category->image)
+            <img src="{{ asset('storage/' . $category->image) }}" class="w-25" alt="50">
+        @endif
+    </div>
+    <div class="form-group">
+        {{-- <label for="">Status</label> --}}
 
 
-        <x-form.radio label="Status" name="status" :checked="$category->status" :options="['active' => 'Active' , 'archived' => 'Archived']" />
-            <div>
-        {{-- <div class="form-check">
+        <x-form.radio label="Status" name="status" :checked="$category->status" :options="['active' => 'Active', 'archived' => 'Archived']" />
+        <div>
+            {{-- <div class="form-check">
             <input class="form-check-input" type="radio" name="status" value="active"
                 @checked(old('status', $category->status)) =='active' )>
             <label class="form-check-label">
@@ -57,9 +65,9 @@
                 Archived
             </label>
         </div> --}}
+        </div>
+
+
+
     </div>
-
-
-
-</div>
-<button type="submit" class="btn btn-success px-5 mb-5">{{ $button_label ?? 'Save' }}</button>
+    <button type="submit" class="btn btn-success px-5 mb-5">{{ $button_label ?? 'Save' }}</button>
