d79c92fbbe3fd8448edf47e06dbbc24c3fe9a4e0
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-04-17 01:45:18 +0300

    init:commit add seeders cate&Prod&Stor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Store;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +22,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(UserSeeder::class);
+        Store::factory(5)->create();
+        Category::factory(10)->create();
+        Product::factory(100)->create();
+        
+       // $this->call(UserSeeder::class);
     }
 }
