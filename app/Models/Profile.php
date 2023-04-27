d6ec8053a423ebccbcd661cfba86b04b0218564e
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-04-28 02:13:22 +0300

    init:commit add fillable

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+
+   // protected $guarded= [];
+
+    protected $fillable = [
+        'user_id' , 'first_name' , 'last_name' , 'birthday' , 'gender' ,
+        'street_address' , 'city' , 'state' , 'postal_code' ,  'country' ,
+        'locale'
+    ];
+    protected $primaryKey= 'user_id';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class , 'user_id' , 'id')->withDefault();
+    }
 }
