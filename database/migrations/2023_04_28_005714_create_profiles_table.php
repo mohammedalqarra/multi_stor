1a0cce36ba095dfe05eb8e44f3211d2395921ae5
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-04-28 02:05:11 +0300

    init:commit add table profile

--- database/migrations/2023_04_28_005714_create_profiles_table.php
+++ database/migrations/2023_04_28_005714_create_profiles_table.php
@@ -12,8 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
+            //$table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->string('street_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->char('country', 2);
+            $table->char('locale', 2)->default('en');
             $table->timestamps();
+
+            $table->primary('user_id');
+
         });
     }
 
