0bd54aec16daede188a963b24e5e721d5c8b03a2
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-04-28 16:44:19 +0300

    init:commit add controller(profile)

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Models\Profile;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -9,4 +10,44 @@ use Illuminate\Support\Facades\Auth;
 class ProfileController extends Controller
 {
     //
+    public function edit()
+    {
+        $user = Auth::user();
+
+        return view('admin.profile.edit' , [
+            'user' =>  $user,
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'first_name' => ['required' , 'string' , 'max:255'],
+            'last_name'  => ['required' , 'string' , 'max:255'],
+            'birthday'   => ['nullable' , 'date' , 'before:today'],
+            'gender'     => ['in:male,female'],
+            'country'    => ['required' , 'string' , 'size:2']
+        ]);
+
+        $user = $request->user();
+
+        $user->profile->fill($request->all())->save();
+
+        return redirect()->route('admin.profile.edit')->with('success' , 'Profile update!');
+        
+        // $user = $request->user();
+
+        // $profile  = $user->profile;
+
+        // if($profile->user_id){
+        //     $profile->update($request->all());
+        // }else {
+        //     $request->merge([
+        //         'user_id' => $user->id,
+        //     ]);
+        //     Profile::create($request->all());
+        // }
+
+       // $user->profile->update($request->all());
+    }
 }
