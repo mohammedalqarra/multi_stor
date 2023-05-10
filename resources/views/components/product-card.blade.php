f7c2a28e7da0ffd6a689cff80491beaf391a0659
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-10 23:24:57 +0300

    init:commit add components product-card

---
+++ resources/views/components/product-card.blade.php
@@ -0,0 +1,25 @@
+<div class="single-product">
+    <div class="product-image">
+        <img src="https://via.placeholder.com/335x335" alt="#">
+        <div class="button">
+            <a href="product-details.html" class="btn"><i class="lni lni-cart"></i> Add to Cart</a>
+        </div>
+    </div>
+    <div class="product-info">
+        <span class="category">Watches</span>
+        <h4 class="title">
+            <a href="product-grids.html">Xiaomi Mi Band 5</a>
+        </h4>
+        <ul class="review">
+            <li><i class="lni lni-star-filled"></i></li>
+            <li><i class="lni lni-star-filled"></i></li>
+            <li><i class="lni lni-star-filled"></i></li>
+            <li><i class="lni lni-star-filled"></i></li>
+            <li><i class="lni lni-star"></i></li>
+            <li><span>4.0 Review(s)</span></li>
+        </ul>
+        <div class="price">
+            <span>$199.00</span>
+        </div>
+    </div>
+</div>
