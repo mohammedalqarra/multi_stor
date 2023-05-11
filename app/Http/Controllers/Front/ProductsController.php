c7d84cdb31a7419eaa0ebd9b949520e9dd1f3d17
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-11 15:14:36 +0300

    init:commit add controller front\products

---
+++ app/Http/Controllers/Front/ProductsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductsController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+
+    public function show(Product $product)
+    {
+
+    }
+}
