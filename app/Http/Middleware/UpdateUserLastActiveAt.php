77c0809871e853968d27956b6c6b19e5d0e519e3
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-12 18:53:40 +0300

    init:commit add UpdateuserLastactiveat(middle)

---
+++ app/Http/Middleware/UpdateUserLastActiveAt.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UpdateUserLastActiveAt
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if($user instanceof User){
+            $user->forceFill([
+                'last_active_at'  =>  Carbon::now(),
+            ])
+            ->save();
+        }
+        return $next($request);
+    }
+}
