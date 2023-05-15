62b519b6af83d1bcae5a415b0b59d25206d1da30
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-15 11:58:11 +0300

    init:commit add ObserverCart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Observers\CartObserver;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $incrementing  = false;
+
+    protected $fillable =  [
+        'cookie_id', 'user_id', 'product_id' , 'quantity' , 'options'
+    ];
+
+    // Events(observer)
+    // creating , created , updating , updated , saving , saved
+    // deleting , deleted , restoring ,  restored , retrieved
+
+    protected static function booted()
+    {
+        static::observe(CartObserver::class);
+        // static::creating(function(Cart $cart){
+        //     $cart->id = Str::uuid();
+        // });
+    }
 }
