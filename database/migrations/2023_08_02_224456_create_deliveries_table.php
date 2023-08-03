b2b9776ed63b9ffe7dac2eb3d392ecda79b234dc
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-08-03 13:45:16 +0300

    init:commit add migrate Deliveries

---
+++ database/migrations/2023_08_02_224456_create_deliveries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->point('current_location')->nullable();
+            // $table->double('lng')->nullable();
+            // $table->double('lat')->nullable();
+            $table->enum('status' , ['in-progress', 'delivered'])->default('in-progress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
